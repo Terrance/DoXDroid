16778a0074b89b7be211200bc6a82388abf18703
Ollie Terrance <ollie.terrance@live.co.uk>
2013-09-26 10:00:04 +0100

    Added due date, repeat classes

--- DoXDroid/src/to/uk/terrance/dox/DueDate.java
+++ DoXDroid/src/to/uk/terrance/dox/DueDate.java
@@ -1,5 +1,27 @@
 package to.uk.terrance.dox;
 
+import java.util.Date;
+
 public class DueDate {
+
+	private Date date;
+	private boolean time;
+	
+	public DueDate(Date date, boolean time) {
+		this.date = date;
+		this.time = time;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public boolean hasTime() {
+		return time;
+	}
+	public void setTime(boolean time) {
+		this.time = time;
+	}
 
 }
--- DoXDroid/src/to/uk/terrance/dox/Repeat.java
+++ DoXDroid/src/to/uk/terrance/dox/Repeat.java
@@ -2,4 +2,25 @@ package to.uk.terrance.dox;
 
 public class Repeat {
 
+	private int days;
+	private boolean fromDue;
+
+	public Repeat(int days, boolean fromDue) {
+		this.days = days;
+		this.fromDue = fromDue;
+	}
+
+	public int getDays() {
+		return days;
+	}
+	public void setDays(int days) {
+		this.days = days;
+	}
+	public boolean isFromDue() {
+		return fromDue;
+	}
+	public void setFromDue(boolean fromDue) {
+		this.fromDue = fromDue;
+	}
+
 }
